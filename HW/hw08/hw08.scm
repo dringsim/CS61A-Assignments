15c6d1810ee6f5fa078b466d04f555640223b4b7
dringsim <dringsim@qq.com>
2026-08-05 23:39:41 +0800

    hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,7 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) (if (null? s) #t (if (null? (car s)) #t (if (null? (cdr s)) #t (if (> (car s) (car (cdr s))) #f (ascending? (cdr s)))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) (if (null? s) s (if (null? (car s)) s (if (pred (car s)) (cons (car s) (my-filter pred (cdr s))) (my-filter pred (cdr s))))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) (if (null? lst1) lst2 (if (null? lst2) lst1 (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) (if (null? s) s (if (null? (car s)) s (cons (car s) (filter (lambda (x) (not (= x (car s)))) (no-repeats (cdr s)))))))
